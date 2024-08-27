b97264224b8779ce04e67420b6daad1b41d3e335
sassani134 <sassani134@gmail.com>
2024-08-27 16:35:50 +0200

    finish Line edit gd script

--- line_edit.gd
+++ line_edit.gd
@@ -1,11 +1,11 @@
 extends LineEdit
 
+func saveObject() -> Dictionary:
+	var dict : Dictionary = {
+		"filepath": get_path(),
+		"savedText": text
+	}
+	return dict
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func loadObject(loadedDict: Dictionary) -> void:
+	text = loadedDict.savedText
